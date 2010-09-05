d83aa5f27b2257907213c9df46138add30884888
Edward George <edwardgeorge@gmail.com>
2010-09-06 00:59:46 +0100

    added connwatch stuff into own module within utils ready for wsgi rewrite.

---
+++ drivel/utils/connwatch.py
@@ -0,0 +1,44 @@
+import errno
+import select
+
+import eventlet
+from eventlet import greenthread
+from eventlet import hubs
+
+
+def poll_check(sock, poll=select.poll, mask=select.POLLHUP|select.POLLERR):
+    p = poll()
+    try:
+        p.register(sock, mask)
+        a = p.poll(0)
+        return bool(a)
+    finally:
+        if hasattr(p, 'close'):
+            p.close()
+
+
+def connectionwatcher(sock, proc_weakref, *exc_args):
+    enums = [errno.EPIPE, errno.ECONNRESET]
+    def kill():
+        p = proc_weakref()
+        if p:
+            greenthread.kill(p, *exc_args)
+    while True:
+        hubs.trampoline(sock, read=True)
+        try:
+            if hasattr(sock, 'read'):
+                data = sock.read()
+            else:
+                data = sock.recv(4096)
+            if not data:
+                kill()
+        except socket.error, e:
+            if e[0] in enums:
+                kill()
+        except IOError, e:
+            if e.errno in enums:
+                kill()
+
+
+def spawn(sock, proc, *exc_args):
+    eventlet.spawn(connectionwatcher, sock, proc, *exc_args)
