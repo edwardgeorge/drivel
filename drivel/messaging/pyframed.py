a4d03920d907e37aae2068e7d50400634cc44513
Edward George <edwardgeorge@gmail.com>
2010-09-16 23:45:00 +0100

    implemented peek() for framed messaging.

--- drivel/messaging/pyframed.py
+++ drivel/messaging/pyframed.py
@@ -16,17 +16,22 @@ class Messaging(object):
     def fileno(self):
         return self.sock.fileno()
 
+    def peek(self):
+        if self._len is None and len(self.buff) >= LEN_HEADER.size:
+            header, self.buff = self.buff[:LEN_HEADER.size], self.buff[LEN_HEADER.size:]
+            (self._len,) = LEN_HEADER.unpack(header)
+        if self._len is not None and self._len <= self.buff:
+            return True
+        return False
+
     def wait(self, timeout=None):
         timer = eventlet.Timeout(timeout) if timeout is not None else None
         try:
             while True:
-                if self._len is not None and self._len <= self.buff:
+                if self.peek():
                     ret, self.buff = self.buff[:self._len], self.buff[self._len:]
                     self._len = None
                     return self._ser.loads(ret)
-                elif self._len is None and len(self.buff) >= LEN_HEADER.size:
-                    header, self.buff = self.buff[:LEN_HEADER.size], self.buff[LEN_HEADER.size:]
-                    (self._len,) = LEN_HEADER.unpack(header)
                 else:
                     self.buff += self.sock.recv(self._bsz)
         except eventlet.Timeout, e:
