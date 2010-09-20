daddaff58d2ffb98a6724031f493e174414c11cb
Edward George <edwardgeorge@gmail.com>
2010-09-20 21:36:49 +0100

    added examples for broker throughput testing.

---
+++ examples/brokertest/server.py
@@ -0,0 +1,31 @@
+import time
+import eventlet
+from drivel.messaging.broker import Broker
+broker = Broker('server')
+broker.connections.listen(('', 8899))
+
+BLOCK = 10000
+
+q = eventlet.Queue()
+def handler():
+    start = None
+    count = 0
+    x = 0
+    while True:
+        event, msg = q.get()
+        if start is None:
+            start = time.time()
+        else:
+            count += 1
+            if count == BLOCK:
+                t = time.time() - start
+                x += 1
+                print '%d\t%s\t%s' % (x, (count/t), t)
+                count = 0
+                start = time.time()
+        event.send('bye')
+
+broker.subscribe('hello', q)
+g = eventlet.spawn(handler)
+eventlet.spawn(broker.listen)
+broker.process()
