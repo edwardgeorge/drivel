1884da2150b4a0c8056f0e66787e10371b54805d
Edward George <edwardgeorge@gmail.com>
2010-09-20 23:55:54 +0100

    moved dotted_import to new importing utils modules.

--- drivel/config.py
+++ drivel/config.py
@@ -1,25 +1,7 @@
 from __future__ import with_statement
 import ConfigParser
 
-
-def dotted_import(name):
-    mod, attr = name.split('.'), []
-    obj = None
-    while mod:
-        try:
-            obj = __import__('.'.join(mod), {}, {}, [''])
-        except ImportError, e:
-            attr.insert(0, mod.pop())
-        else:
-            for a in attr:
-                try:
-                    obj = getattr(obj, a)
-                except AttributeError, e:
-                    raise AttributeError('could not get attribute %s from %s'
-                        ' -> %s (%r)' % (a, '.'.join(mod), '.'.join(attr),
-                        obj))
-            return obj
-    raise ImportError('could not import %s' % name)
+from drivel.utils.importing import dotted_import
 
 
 class Config(dict):
