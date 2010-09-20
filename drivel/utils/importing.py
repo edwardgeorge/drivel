de1b383fe85112cb0de821174f51eafbee6d33ee
Edward George <edwardgeorge@gmail.com>
2010-09-20 23:51:35 +0100

    created import_preferential function in utils.importing

---
+++ drivel/utils/importing.py
@@ -0,0 +1,10 @@
+def import_preferential(name, *morenames):
+    if not isinstance(name, (list, tuple)):
+        name = [name]
+    names = list(name) + list(morenames)
+    while names:
+        name = names.pop(0)
+        try:
+            return __import__(name, {}, {}, [''])
+        except ImportError, e:
+            pass
