a770efa9592a271d400e44105f8cd2c575cd6a5e
Edward George <edwardgeorge@gmail.com>
2010-09-23 17:57:09 +0100

    moved static file server to own module.

--- drivel/server.py
+++ drivel/server.py
@@ -62,34 +62,6 @@ class dummylog(object):
         pass
 
 
-class StaticFileServer(object):
-    """For testing purposes only. Use a real static file server.
-    """
-    def __init__(self, directory_list, wrapped_app, host):
-        self.host = host
-        self.host.log("httpd", "info", "serving static files: %s" %
-            directory_list)
-        self.directory_list = [os.path.realpath(x) for x in directory_list]
-        self.wrapped_app = wrapped_app
-
-    def __call__(self, env, start_response):
-        for directory in self.directory_list:
-            path = os.path.realpath(directory + env['PATH_INFO'])
-            if not path.startswith(directory):
-                start_response("403 Forbidden",
-                    [('Content-Type', 'text/plain')])
-                return ['Forbidden']
-            if os.path.isdir(path):
-                path = os.path.join(path, 'index.html')
-            if os.path.exists(path):
-                content_type, encoding = mimetypes.guess_type(path)
-                if content_type is None:
-                    content_type = 'text/plain'
-                start_response("200 OK", [('Content-Type', content_type)])
-                return file(path).read()
-        return self.wrapped_app(env, start_response)
-
-
 class Server(object):
     def __init__(self, config, options):
         self.config = config
@@ -149,6 +121,7 @@ class Server(object):
         app = create_application(self)
         dirs = self.config.server.get('static_directories', None)
         if dirs is not None:
+            from drivel.contrib.fileserver import StaticFileServer
             app = StaticFileServer(dirs.split(','), app, self)
         self.wsgiapp = app
         if start_listeners:
