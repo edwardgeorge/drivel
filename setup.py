bdb160afb2884879350f9043d0fad1646543b8f8
Edward George <edwardgeorge@gmail.com>
2013-03-14 20:59:40 +0000

    update setup.py for changes

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
-import sys
 from setuptools import setup, find_packages
-
-requires=['eventlet', 'WebOb', 'python-daemon']
-if sys.version_info < (2, 6):
-    requires.append('simplejson')
 
 setup(
     name='drivel',
     version='0.1',
-    packages=['drivel'],  # find_packages(exclude=['tests']),
+    packages=find_packages(exclude=['tests']),
     test_suite='nose.collector',
-    install_requires=requires,
-    test_requires=['nose', 'mock', 'WebTest'],
+    install_requires=['eventlet'],
+    test_requires=['nose', 'mock'],
 )
