f37765fab3462b26a92e82eda7d4d3da79ab459a
Edward George <edwardgeorge@gmail.com>
2013-03-15 11:50:49 +0000

    make sure import_preferential raises ImportError if nothing imported

--- drivel/utils/importing.py
+++ drivel/utils/importing.py
@@ -2,9 +2,11 @@ def import_preferential(name, *morenames):
     if not isinstance(name, (list, tuple)):
         name = [name]
     names = list(name) + list(morenames)
+    namestr = ', '.join(names[:])
     while names:
         name = names.pop(0)
         try:
             return __import__(name, {}, {}, [''])
         except ImportError:
             pass
+    raise ImportError('Could not import anything from %s' % (namestr, ))
